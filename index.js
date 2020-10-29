dc2fd086ff3adfb3dcee44c416b37eb0e0367ac4
Rohan2596 <rohankadam662@gmail.com>
2020-10-29 16:12:37 +0530

    ADDED:Basic routes for CRU operation on events.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express=require('express')
-const app=express()
+const express = require('express');
+const app = express();
+
+
+
+//Making Get Request  to fetch all events
+app.get('/events', async (request, response) => {
+    response.send("Getting All Events Registered.")
+});
+
+//Making Post Request to add events to database
+app.post('/event/add', async (request, response) => {
+    response.send("Adding Events to Database.")
+});
+
+
+//making Put request to update events to database
+app.put('/events/update', async (request, response) => {
+    response.send("Updating Events on to Database.")
+});
+
+
+
+
 //And finally listening to that particular server.
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is listening to port');
-    
+
 })

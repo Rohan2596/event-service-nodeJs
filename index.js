f2f2f99dde0deef347beb15351ffe7b10f59a640
Rohan2596 <rohankadam662@gmail.com>
2020-10-29 14:14:45 +0530

    ADDED:Express module for creating server.

---
+++ index.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const app=express()
+//And finally listening to that particular server.
+app.listen(3000,()=>{
+    console.log('Server is listening to port');
+    
+})

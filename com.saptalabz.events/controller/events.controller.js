21726a0ccb329ec029316d08c62ab9516220e4d7
Rohan2596 <rohankadam662@gmail.com>
2020-10-31 12:06:37 +0530

    ADDED:Validations Check erors for input field.

--- com.saptalabz.events/controller/events.controller.js
+++ com.saptalabz.events/controller/events.controller.js
@@ -1,30 +1,65 @@
+const { RSA_NO_PADDING } = require("constants");
+
+const response = {
+    success: "false",
+    message: "Detail Empty",
+}
 module.exports = {
 
     getAllEvents: async (request, response) => {
 
-
-        response.status(200).send("Adding Events to database.")
+        try {
+            response.status(200).send("Adding Events to database.")
+        } catch (error) {
+            response.status(404).send("Failed to Add Events to database.")
+        }
 
     },
     addingEvent: async (request, response) => {
         try {
-           request.checkBody('title','Title Cannot be empty').notEmpty();
-           
-           var error=request.validationErrors();
-           if(error){
-            response.status(422).send("Adding Events to database." )
-           }else{
-            response.status(200).send("Adding Events to database." )
-           }
-            
-        } catch (error){
+            request.checkBody('title', 'Invalid Events Title!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('description', 'Invalid Events Description!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('type', 'Invalid Events Type!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('location', 'Invalid Events Location!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('date', 'Invalid Events date!').notEmpty().isAlpha().isLength({ min: '2' });
+
+
+            let validationErrors = request.validationErrors();
+            if (validationErrors) {
+                response.status(500).send("Validation Errors..")
+            } else {
+                response.status(200).send("Adding Events to database.")
+
+            }
+
+        } catch (error) {
             console.log(error);
-            
+
             response.status(404).send("Failed to Add Events to database.")
         }
     },
     updateEvent: async (request, response) => {
-        response.status(200).send("Adding Events to database." )
+        try {
+            request.checkBody('title', 'Invalid Events Title!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('description', 'Invalid Events Description!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('type', 'Invalid Events Type!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('location', 'Invalid Events Location!').notEmpty().isAlpha().isLength({ min: '2' });
+            request.checkBody('date', 'Invalid Events date!').notEmpty().isAlpha().isLength({ min: '2' });
+
+
+            let validationErrors = request.validationErrors();
+            if (validationErrors) {
+                response.status(500).send("Validation Errors..")
+            } else {
+                response.status(200).send("Updating  Events to database.")
+
+            }
+
+        } catch (error) {
+            console.log(error);
+
+            response.status(404).send("Failed to update Events to database.")
+        }
     }
 
 
